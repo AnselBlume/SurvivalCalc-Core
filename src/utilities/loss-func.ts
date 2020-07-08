24b170ba2b07419885d7b6455cc003fea7bd8695
Ansel Blume <anselblume@gmail.com>
2020-07-08 03:08:53 -0700

    Add BulkLoss

--- src/utilities/loss-func.ts
+++ src/utilities/loss-func.ts
@@ -1,5 +1,6 @@
 import { Spread } from 'model/spread';
 import { Stat } from 'model/stat';
+import { Pokemon, calcStat, Generations } from '@smogon/calc';
 
 /**
  * A class whose loss function determines how bad a Spread is.
@@ -8,18 +9,27 @@ export interface LossFunction {
     loss(spread: Spread): number;
 }
 
+/**
+ * Loss function to minimize HP
+ */
 export class MinHPLoss implements LossFunction {
     loss(spread: Spread): number {
         return spread[Stat.HP];
     }
 }
 
+/**
+ * Loss function to maximize HP
+ */
 export class MaxHPLoss implements LossFunction {
     loss(spread: Spread): number {
         return -spread[Stat.HP];
     }
 }
 
+/**
+ * Loss function to minimize EVs while minimizing or maximizing HP
+ */
 export class MinEVsLoss implements LossFunction {
     mode: MinHPLoss | MaxHPLoss;
 
@@ -34,5 +44,45 @@ export class MinEVsLoss implements LossFunction {
         // over minimizing/maximizing HP as [Min,Max]imizeHP returns a number with abs
         // value in {0, ... , 255}
         return 1000 * evTotal + this.mode.loss(spread);
+    }
+}
+
+/**
+ * Loss function to find spreads with the best overall bulk. The weight parameter in [0, 1] controls
+ * how much attention is paid to the def/sDef side, with a weight of 0 maximizing defensive bulk
+ * and a weight of 1 maximizing special defensive bulk. A weight of .5 balances the bulk equally.
+ *
+ * This loss function was derived in a similar fashion to X-Act's original here:
+ * https://www.smogon.com/dp/articles/maximizing_defenses
+ */
+export class BulkLoss implements LossFunction {
+    defender: Pokemon;
+    damageConst: number;
+    weight: number;
+
+    constructor(defender: Pokemon, weight: number) {
+        if (weight < 0 || weight > 1) {
+            throw new TypeError('weight argument must be in [0, 1]');
+        }
+
+        this.defender = defender;
+        this.weight = weight;
+
+        // Compute damage constant
+        // Assume attacker has same level as defender, base 100 power move, base 120 attack and boosting nature
+        this.damageConst = (2 * defender.level / 5 + 2) * 2
+                           * calcStat(Generations.get(8), Stat.ATK, 120, 31, 252, defender.level, 'Adamant');
+    }
+
+    loss(spread: Spread): number {
+        const hpStat = calcStat(Generations.get(8), Stat.HP, this.defender.species.baseStats[Stat.HP],
+                                this.defender.ivs[Stat.HP], spread[Stat.HP], this.defender.level, this.defender.nature);
+        const defStat = calcStat(Generations.get(8), Stat.DEF, this.defender.species.baseStats[Stat.DEF],
+                                 this.defender.ivs[Stat.DEF], spread[Stat.DEF], this.defender.level, this.defender.nature);
+        const sDefStat = calcStat(Generations.get(8), Stat.SDEF, this.defender.species.baseStats[Stat.SDEF],
+                                  this.defender.ivs[Stat.SDEF], spread[Stat.SDEF], this.defender.level, this.defender.nature);
+
+
+        return (this.damageConst * ((1 - this.weight) / defStat + this.weight / sDefStat) + 2) / hpStat;
     }
 }
--- test/utilities/loss-func.test.ts
+++ test/utilities/loss-func.test.ts
@@ -1,5 +1,6 @@
 import * as lossFuncs from 'utilities/loss-func';
 import { Spread } from 'model';
+import { Pokemon, Generations } from '@smogon/calc';
 
 describe('Loss Function Unit Tests', () => {
     const spread1 = new Spread(20, 76, 128);
@@ -36,5 +37,37 @@ describe('Loss Function Unit Tests', () => {
         expect(minEVs.loss(spread1)).toBe(223980);
         expect(minEVs.loss(spread2)).toBe(471948);
         expect(minEVs.loss(spread3)).toBe(507748);
+    });
+
+    test('BulkLoss with weight = 0', () => {
+        const kyogre = new Pokemon(Generations.get(8), 'Kyogre', { level: 50 });
+        const bulkLoss = new lossFuncs.BulkLoss(kyogre, 0);
+
+        expect(bulkLoss.loss(spread1)).toBeCloseTo(0.400562, 4);
+        expect(bulkLoss.loss(spread2)).toBeCloseTo(0.335048, 4);
+        expect(bulkLoss.loss(spread3)).toBeCloseTo(0.371589, 4);
+    });
+
+    test('BulkLoss with weight = 1', () => {
+        const groudon = new Pokemon(Generations.get(8), 'Groudon', { level: 100 });
+        const bulkLoss = new lossFuncs.BulkLoss(groudon, 1);
+
+        expect(bulkLoss.loss(spread1)).toBeCloseTo(0.369942, 4);
+        expect(bulkLoss.loss(spread2)).toBeCloseTo(0.345154, 4);
+        expect(bulkLoss.loss(spread3)).toBeCloseTo(0.282178, 4);
+    });
+
+    test('BulkLoss with weight = 0.5', () => {
+        const rayquaza = new Pokemon(Generations.get(8), 'Rayquaza', { level: 75 });
+        const bulkLoss = new lossFuncs.BulkLoss(rayquaza, .5);
+
+        expect(bulkLoss.loss(spread1)).toBeCloseTo(0.373739, 4);
+        expect(bulkLoss.loss(spread2)).toBeCloseTo(0.329400, 4);
+        expect(bulkLoss.loss(spread3)).toBeCloseTo(0.317255, 4);
+    });
+
+    test('BulkLoss invalid weight', () => {
+        expect(() => new lossFuncs.BulkLoss(new Pokemon(Generations.get(8), 'Kyogre'), 1.1)).toThrow(TypeError);
+        expect(() => new lossFuncs.BulkLoss(new Pokemon(Generations.get(8), 'Kyogre'), -.1)).toThrow(TypeError);
     });
 });
