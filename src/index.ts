5de4b79db37274e3447a69165bbeed4cfd0606eb
Ansel Blume <anselblume@gmail.com>
2020-07-17 11:59:11 -0700

    Convenience webpack.config.js

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export * from './min-evs';
+export * from './best-evs';
+export * from './model';
+export * from '@smogon/calc';
+
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin'); // To resolve baseUrl in tsconfig
+
+module.exports = {
+  entry: './src/index.ts',
+  mode: 'production',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ],
+    plugins: [new TsconfigPathsPlugin({ configFile: './tsconfig.json' })]
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
