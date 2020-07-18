ba42cfa2e57b1330a5760e634eaa50c224e65454
Ansel Blume <anselblume@gmail.com>
2020-07-17 17:30:44 -0700

    Add documentation for findMinEVs and findBestEVs

--- src/best-evs.ts
+++ src/best-evs.ts
@@ -2,6 +2,14 @@ import { Requirements, Spread, Attack } from 'model';
 import { BulkLoss, SpreadComparator, validateInput, SpreadGenerator, meetsReqs, applySpread } from 'utilities';
 import { Pokemon } from '@smogon/calc';
 
+/**
+ * Finds the best EV spread which meets the specified Requirements and uses up to maxEVs EVs, while
+ * minimizing the damage from the Attack[] or minimizing the BulkLoss.
+ *
+ * @param reqs The Requirements to be met.
+ * @param toMinimize An array of Attacks or a BulkLoss to minimize.
+ * @param maxEVs The total number of usable EVs.
+ */
 export function findBestEVs(reqs: Requirements, toMinimize: Attack[] | BulkLoss, maxEVs: number): Spread {
     validateInput(reqs);
 
--- src/min-evs.ts
+++ src/min-evs.ts
@@ -4,6 +4,11 @@ import { MoveCategory } from '@smogon/calc/dist/data/interface';
 import { MinEVsLoss, MinHPLoss, MaxHPLoss } from 'utilities/loss-func';
 import { validateInput, SpreadGenerator, SpreadComparator, meetsReqs, applySpread } from 'utilities';
 
+/**
+ * Finds the EV spread which meets the requirements which uses the fewest total EVs.
+ *
+ * @param requirements The Requirements to be met.
+ */
 export function findMinEVs(requirements: Requirements): Spread {
     validateInput(requirements);
 
@@ -34,7 +39,7 @@ export function findMinEVs(requirements: Requirements): Spread {
     const minimizingHP = hpLossFunc instanceof MinHPLoss; // Are we minimizing HP
 
     for (const spread of spreads) {
-        // We could simply check whether each spread meets the requirements then let the SpreadComparator 
+        // We could simply check whether each spread meets the requirements then let the SpreadComparator
         // use the MinimizeEVs loss function to find the best spread. However, calling meetsReqs on every
         // spread is expensive. Check whether the spread has fewer EVs than the best first and if the HP
         // is either higher or lower than the best, depending on whether we're maximizing or minimizing it
@@ -46,10 +51,10 @@ export function findMinEVs(requirements: Requirements): Spread {
         if (evTotal > bestEVTotal || evTotal === bestEVTotal && hasBadHP) {
             continue;
         }
-        
+
         // Apply spread to each defender
         requirements.survivalReqs.forEach(survivalReq => applySpread(spread, survivalReq.attacks));
-        
+
         if (meetsReqs(requirements) && comparator.ingestSpread(spread)) { // Meets requirements and is better
             bestEVTotal = evTotal;
         }
@@ -64,7 +69,7 @@ export function findMinEVs(requirements: Requirements): Spread {
 
 /**
  * Checks whether each Attack of each SurvivalRequirement does damage to the same defensive side.
- * 
+ *
  * @param survivalReqs The array of SurvivalRequirements to check.
  */
 function hasSameDefensiveCategory(survivalReqs: SurvivalRequirement[]): boolean {
@@ -83,7 +88,7 @@ function hasSameDefensiveCategory(survivalReqs: SurvivalRequirement[]): boolean 
 
 /**
  * Gets the defensive Stat corresponding to the defensive category shared by all attacks.
- * 
+ *
  * @param survivalReqs The SurvivalRequirements to extract the defensive category from.
  * @throws A TypeError if the defensive category is 'Status'.
  */
